4009eb113f4d7239bb8421a40df2fd3d54f4d398
Muriel Silveira <murielkong@gmail.com>
2020-01-18 16:51:24 +0100

    chore: create a simple graphql api

--- src/server.js
+++ src/server.js
@@ -1,8 +1,31 @@
 const fastify = require('fastify')
+const fatifyGraghQL = require('fastify-gql')
 const app = fastify()
 
 app.get('/', async () => {
   return { hey: 'yo' }
+})
+
+const gql = args => (args[0])
+
+const schema = gql`
+  type Query {
+    hey(name: String): String
+  }
+`
+
+const resolvers = {
+  Query: {
+    hey: async (root, { name = ''}) => {
+      return `Yo ${name}`.trim()
+    }
+  }
+}
+
+app.register(fatifyGraghQL, {
+  schema,
+  resolvers,
+  graphiql: 'playground',
 })
 
 const port = process.env.PORT || 3003
