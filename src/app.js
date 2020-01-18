0b3e0d464b9ab919ceaf910c2905db882d8c5f1f
Muriel Silveira <murielkong@gmail.com>
2020-01-18 16:55:04 +0100

    chore: move fastify app to it's own file

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const fastify = require('fastify')
+const fatifyGraghQL = require('fastify-gql')
+const app = fastify()
+
+app.get('/', async () => {
+  return { hey: 'yo' }
+})
+
+const gql = args => (args[0])
+
+const schema = gql`
+  type Query {
+    hey(name: String): String
+  }
+`
+
+const resolvers = {
+  Query: {
+    hey: async (_, { name = ''}) => {
+      return `Yo ${name}`.trim()
+    }
+  }
+}
+
+app.register(fatifyGraghQL, {
+  schema,
+  resolvers,
+  graphiql: 'playground',
+})
+
+module.exports = app
--- src/server.js
+++ src/server.js
@@ -1,34 +1,7 @@
-const fastify = require('fastify')
-const fatifyGraghQL = require('fastify-gql')
-const app = fastify()
-
-app.get('/', async () => {
-  return { hey: 'yo' }
-})
-
-const gql = args => (args[0])
-
-const schema = gql`
-  type Query {
-    hey(name: String): String
-  }
-`
-
-const resolvers = {
-  Query: {
-    hey: async (root, { name = ''}) => {
-      return `Yo ${name}`.trim()
-    }
-  }
-}
-
-app.register(fatifyGraghQL, {
-  schema,
-  resolvers,
-  graphiql: 'playground',
-})
+const app = require('./app')
 
 const port = process.env.PORT || 3003
+
 app.listen(port, error => {
   if (error) {
     console.error(error)
